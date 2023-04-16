7b75299d1fd89eafd0f3a6c38c1022898b585612
Anat Gindin <anat.gindin@campus.technion.ac.ilgit config --global user.name anat.gindingit config --global user.name anatGindin>
2023-04-16 17:54:23 +0300

    right number

--- print_number.c
+++ print_number.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
-int course_number[6] = {4, 2, 1, 4, 3, 2};
+#define LEN 6
+int course_number[LEN] = {4, 2, 1, 4, 3, 2};
 int main() {
-	for(int i = 6; i > 0; i--) {
+	for(int i = LEN-1; i >= 0; i--) {
 		printf("%d", course_number[i]);
 	}
 	printf("\n");
